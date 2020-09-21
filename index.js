c86860436e874739a21472e0148f597cc847dbf4
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-09-21 03:06:56 +0300

    finished rudimentary auth

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const server = require('./src/server.js');
+const MONGOOSE_URL = process.env.MONGOOSE_URL;
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGOOSE_URL, mongooseOptions);
+
+server.start(4000);
---
+++ src/server.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// requirements constants
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const notFoundHandler = require('./middleware/404');
+const serverErrorHandler = require('./middleware/500');
+// const getModel = require('./middleware/model-finder');
+const usersModel = require('./auth/models/users-model');
+const basicAuth = require('./auth/middleware/basic');
+
+
+// Global MiddleWare where you could call it anywhere and has a global scope
+app.use(express.json());
+app.use(cors());
+app.use(serverErrorHandler);
+
+// routes as MiddleWare
+// generic model
+app.post('/signup', postAuthDetails);
+app.post('/signin', basicAuth, verifyAuthDetails);
+
+app.get('/users', getUserDetails);
+// get model
+// app.param('model', getModel);
+
+// error routes
+app.use('*', notFoundHandler);
+
+// ----------------------------------- functions categories ----------------------------------- //
+async function postAuthDetails(req, res, next) {
+  console.log('here in post');
+  usersModel.create(req.body).then(user => {
+    res.status(200).send(user);  
+  }).catch(err=> {
+    console.log(err);
+    next(err);
+  });
+}
+
+function verifyAuthDetails(req, res, next) {
+  if (req.token) {
+    res.status(200).send({
+      token: req.token,
+      user: req.user
+    });
+  } else {
+    res.status(401).send('User Does Not Exists!');
+  }
+}
+
+function getUserDetails(req, res, next) {
+  usersModel.get().then(data => {
+    let output = {
+      count: 0,
+      results: [],
+    };
+
+    output.count = data.length;
+    output.results = data;
+    res.status(200).json(output);
+  }).catch(err=> {
+    console.log(err);
+    next(err);
+  });
+}
+
+module.exports = {
+  server: app, 
+  start: port => {
+    let PORT = port || process.env.port || 3000;
+    app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
+  },
+};
